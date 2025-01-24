2fe0e805e28c06d6ee5035159ba238b6d238c8b5
kalaiarasan1204 <kalai1204arasan@gmail.com>
2025-01-24 19:47:41 +0530

    Create add two numbers.py

---
+++ add two numbers.py
@@ -0,0 +1,7 @@
+# get the values from the user
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+# to add the two values
+result = num1 + num2
+# to print the vlues
+print(f"the sum of {num1} and {num2} is {result}")
